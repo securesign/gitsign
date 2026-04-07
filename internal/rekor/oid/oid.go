3154eb64830cf3ff0f40da41497317849b8c994d
Tommy Dalton <tdalton@redhat.com>
2026-04-07 09:24:02 +0100

    Attempting to fix lints

--- internal/rekor/oid/oid.go
+++ internal/rekor/oid/oid.go
@@ -113,7 +113,7 @@ func ToAttributes(tlog *models.LogEntryAnon) (protocol.Attributes, error) {
 		return nil, err
 	}
 
-	attrs := make(protocol.Attributes, 2)
+	attrs := protocol.Attributes{} //nolint:errcheck
 	attr, err := protocol.NewAttribute(OIDRekorTransparencyLogEntry, out)
 	if err != nil {
 		return nil, err
